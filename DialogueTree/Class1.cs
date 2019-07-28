3f33101c86fe059ca0dc7d73a9becd040565f127
phxtho <phathopukwana@gmail.com>
2019-07-28 23:01:03 +0200

    dialogue node skeleton

--- DialogueTree/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DialogueTree
-{
-    public class Class1
-    {
-    }
-}
---
+++ DialogueTree/DialogueNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DialogueTree
+{
+    public class DialogueNode
+    {
+        public int nodeId;
+
+        public string _text;
+
+        //public List<T> options;
+
+        DialogueNode() { }
+
+        DialogueNode(string text)
+        {
+            _text = text;
+        }
+    }
+}
